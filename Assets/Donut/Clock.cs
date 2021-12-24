5198584ff45f6ff70c784e48b24fbe568ab59c2b
Kaede Fujisaki <kaede@hexe.net>
2021-12-25 08:23:35 +0900

    Impl clock

--- Assets/Donut/Clock.cs
+++ Assets/Donut/Clock.cs
@@ -1,6 +1,20 @@
+using UnityEngine;
+
 namespace Donut {
-  public class Clock {
+  public class Clock : MonoBehaviour {
+    private uint leaps_;
+    private uint current_;
     public Clock() {
+      this.leaps_ = 0;
+      this.current_ = 0;
+    }
+
+    public void tick() {
+      this.current_++;
+    }
+
+    public void back() {
+      this.current_--;
     }
   }
 }
