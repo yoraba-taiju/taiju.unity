4933eedae3095175cf46183b5fbde0444014e2a7
Kaede Fujisaki <kaede@hexe.net>
2021-12-26 18:14:30 +0900

    move witch

--- Assets/Donut/Clock.cs
+++ Assets/Donut/Clock.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 namespace Donut {
   public class Clock : MonoBehaviour {
+    const uint RECORD_LEN = 600;
     private uint leaps_;
     private uint current_;
+    private PlayableDirector playableDirector_;
+    
+    private readonly double[] times_ = new double[RECORD_LEN];
+
+    private void Start() {
+      playableDirector_ = gameObject.GetComponent<PlayableDirector>();
+    }
+
+    private void Update() {
+      this.
+      tick();
+    }
+
     public Clock() {
       this.leaps_ = 0;
       this.current_ = 0;
     }
 
     public void tick() {
-      this.current_++;
+      current_++;
     }
 
     public void back() {
-      this.current_--;
+      current_--;
     }
   }
 }
---
+++ Assets/Lib/Witch.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+namespace Lib {
+  public class Witch : MonoBehaviour {
+    private PlayerInput playerInput_;
+    void Start() {
+      playerInput_ = new PlayerInput();
+      playerInput_.Enable();
+      //playerInput_.Player.Move.performed += context => Debug.Log($"{context.ReadValue<Vector2>()}");
+    }
+
+    // Update is called once per frame
+    void Update() {
+      var move = playerInput_.Player.Move.ReadValue<Vector2>() * Time.deltaTime * 7;
+      var trans = transform;
+      var pos = trans.position;
+      pos.x += move.x;
+      pos.y += move.y;
+      trans.position = pos;
+      //Debug.Log($"{move.x}, {move.y}");
+    }
+  }
+}
