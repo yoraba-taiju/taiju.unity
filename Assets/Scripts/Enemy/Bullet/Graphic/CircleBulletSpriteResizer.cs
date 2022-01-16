af5a0bc831eda3ab075bf1815cfce104e18e82f2
Kaede Fujisaki <kaede@hexe.net>
2022-01-16 15:10:26 +0900

    rename

--- Assets/Scripts/Enemy/Bullet/Graphic/CircleBulletSpriteResizer.cs
+++ Assets/Scripts/Enemy/Bullet/Graphic/CircleBulletSpriteResizer.cs
@@ -2,15 +2,15 @@
 using Donut.Unity;
 using UnityEngine;
 
-namespace Enemy.Bullet {
-  public sealed class BulletSpriteResizer : DonutBehaviour {
+namespace Enemy.Bullet.Graphic {
+  public sealed class CircleBulletSpriteResizer : DonutBehaviour {
     [SerializeField] public GameObject foreground;
 
     protected override void OnStart() {
     }
 
     protected override void OnUpdate() {
-      var scale = 0.85f + ((float)Math.Sin(clock.CurrentTick / 30.0f * Math.PI * 2) * 0.05f);
+      var scale = 0.88f + ((float)Math.Sin(clock.CurrentTick / 30.0f * Math.PI * 2) * 0.05f);
       foreground.transform.localScale = Vector3.one * scale;
     }
   }
