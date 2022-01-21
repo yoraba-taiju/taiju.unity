631f5c85e47f1ef2691e416fdc4cc65be9bb7212
Kaede Fujisaki <kaede@hexe.net>
2022-01-21 18:56:23 +0900

    Refactor

--- Assets/Scripts/Donut/Unity/TransformWithHistory.cs
+++ Assets/Scripts/Donut/Unity/TransformWithHistory.cs
@@ -1,48 +1,59 @@
-﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
+﻿using System.Linq;
 using Donut.Values;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Donut.Unity {
   public class TransformWithHistory: MonoBehaviour {
+    // Clock
+    private ClockComponent clockComponent_;
+    private Clock clock_;
+
+    // Visibility
+    [SerializeField] public bool destroyWhenInvisible = true;
+    private Renderer[] renderers_;
+    private bool wasVisible_;
+
+    // Transform records
     private Dense<Vector3> position_;
     private Dense<Vector3> scale_;
     private Dense<Quaternion> rot_;
-    private ClockComponent clockComponent_;
-    private Clock clock_;
-    private Renderer[] renderers_;
-    private bool wasVisible_ = false;
     private void Start() {
       var obj = GameObject.FindGameObjectWithTag("Clock");
       clockComponent_ = obj.GetComponent<ClockComponent>();
       clock_ = clockComponent_.Clock;
+      if (destroyWhenInvisible) {
+        renderers_ = GetComponentsInChildren<Renderer>();
+        wasVisible_ = renderers_.All(it => it.isVisible);
+      }
       var trans = transform;
       position_ = new Dense<Vector3>(clock_, trans.localPosition);
       scale_ = new Dense<Vector3>(clock_, trans.localScale);
       rot_ = new Dense<Quaternion>(clock_, trans.localRotation);
-      renderers_ = GetComponentsInChildren<Renderer>();
-      wasVisible_ = renderers_.All(it => it.isVisible);
     }
 
     private void Update() {
-      if (wasVisible_) {
-        if (!renderers_.Any(it => it.isVisible)) {
-          Destroy(gameObject);
+      if(destroyWhenInvisible) { // Visibility Management
+        var visible = renderers_.Any(it => it.isVisible);
+        if (wasVisible_) {
+          if (!visible) {
+            // TODO
+            Destroy(gameObject);
+          }
+        } else {
+          wasVisible_ = visible;
         }
-      } else {
-        wasVisible_ = renderers_.All(it => it.isVisible);
       }
-      var trans = transform;
-      if (clock_.IsTicking) {
-        position_.Value = trans.localPosition;
-        scale_.Value = trans.localScale;
-        rot_.Value = trans.localRotation;
-      } else {
-        trans.localPosition = position_.Value;
-        trans.localScale = scale_.Value;
-        trans.localRotation = rot_.Value;
+      { // Manage transforms
+        var trans = transform;
+        if (clock_.IsTicking) {
+          position_.Value = trans.localPosition;
+          scale_.Value = trans.localScale;
+          rot_.Value = trans.localRotation;
+        } else {
+          trans.localPosition = position_.Value;
+          trans.localScale = scale_.Value;
+          trans.localRotation = rot_.Value;
+        }
       }
     }
   }
