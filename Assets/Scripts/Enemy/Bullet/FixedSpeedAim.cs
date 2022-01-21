17d92217359fb0a0007e2df72956f6a59b31a2bd
Kaede Fujisaki <kaede@hexe.net>
2022-01-21 18:11:00 +0900

    treas 0 or null

--- Assets/Scripts/Enemy/Bullet/FixedSpeedAim.cs
+++ Assets/Scripts/Enemy/Bullet/FixedSpeedAim.cs
@@ -9,13 +9,22 @@ namespace Enemy.Bullet {
     private Vector3 direction_;
     protected override void OnStart() {
       sora_ = GameObject.FindWithTag("Player");
+      if (sora_ == null) {
+        direction_ = Vector3.left * speed;
+        return;
+      }
       var trans = transform;
       var pos = trans.position;
       var vec = sora_.transform.position - pos;
       vec.z = 0.0f;
-      vec /= vec.magnitude;
-      direction_ = vec * speed;
-      direction_ = Quaternion.AngleAxis(angle, Vector3.forward) * direction_;
+      var magnitude = vec.magnitude;
+      if (magnitude > float.Epsilon) {
+        vec /= magnitude;
+        direction_ = vec * speed;
+        direction_ = Quaternion.AngleAxis(angle, Vector3.forward) * direction_;
+      } else {
+        direction_ = Vector3.left * speed;
+      }
     }
 
     protected override void OnUpdate() {
