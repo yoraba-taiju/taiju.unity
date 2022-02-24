747f564b3b79675970190b6c1464bf31cdfcb5f2
Kaede Fujisaki <kaede@hexe.net>
2022-02-24 09:46:10 +0900

    Scroll forest using script.

---
+++ Assets/Scripts/Background/Stage01/ForestScroll.cs
@@ -0,0 +1,15 @@
+﻿using Donut.Unity;
+using UnityEngine;
+
+namespace Background.Stage01 {
+  public class ForestScroll : DonutBehaviour {
+    private Renderer renderer_;
+    protected override void OnStart() {
+      renderer_ = GetComponent<Renderer>();
+    }
+
+    protected override void OnUpdate() {
+      renderer_.material.mainTextureOffset -= Vector2.one * Time.deltaTime * 0.1f;
+    }
+  }
+}
