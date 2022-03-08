b0d71618325c3b44e7f44d2bbc0ab13e32b9c9cb
Kaede Fujisaki <kaede@hexe.net>
2022-03-09 00:39:31 +0900

    SpawnEmitter is now just a marker.

--- Assets/Scripts/Level/SpawnEmitter.cs
+++ Assets/Scripts/Level/SpawnEmitter.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
 namespace Level {
-  public class SpawnEmitter : SignalEmitter
-  {
+  [Serializable]
+  public class SpawnEmitter : Marker, INotification {
     [SerializeField]
     public GameObject prefab;
 
     [SerializeField]
     public Vector2 position;
 
-    public void OnEnable() {
-      // Fill in empty signal asset.
-      asset = ScriptableObject.CreateInstance<SignalAsset>();
-    }
+    public PropertyName id => new("Enemy Spawn");
   }
 }
