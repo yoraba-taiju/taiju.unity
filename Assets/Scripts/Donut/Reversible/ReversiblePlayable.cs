88dedd4183efdb940e2fd462ec0e92416c5dca91
Kaede Fujisaki <kaede@hexe.net>
2022-03-17 19:28:58 +0900

    Fix ReversiblePlayable correctly

--- Assets/Scripts/Donut/Reversible/ReversiblePlayable.cs
+++ Assets/Scripts/Donut/Reversible/ReversiblePlayable.cs
@@ -11,15 +11,23 @@ namespace Donut.Reversible {
     private Clock clock_;
     private PlayableDirector playableDirector_;
     private uint bornAt_;
+    
+    private struct State {
+      public double time;
+      public bool isPlaying;
+    }
 
     // Transform records
-    private Dense<double> time_;
+    private Dense<State> states_;
     private void Start() {
       holder_ = GameObject.FindGameObjectWithTag("Clock").GetComponent<ClockHolder>();
       clock_ = holder_.Clock;
       bornAt_ = clock_.CurrentTick;
       playableDirector_ = gameObject.GetComponent<PlayableDirector>();
-      time_ = new Dense<double>(clock_, playableDirector_.time);
+      states_ = new Dense<State>(clock_, new State {
+        time = playableDirector_.time,
+        isPlaying = playableDirector_.state == PlayState.Playing,
+      });
     }
 
     private void Update() {
@@ -28,13 +36,18 @@ namespace Donut.Reversible {
         return;
       }
       if (holder_.Ticked) {
-        time_.Mut = playableDirector_.time;
+        ref var state = ref states_.Mut;
+        state.time = playableDirector_.time;
+        state.isPlaying = playableDirector_.state == PlayState.Playing;
       } else if (holder_.Backed) {
         // Debug.Log($"Back: {clock_.CurrentTick}: {playableDirector_.time} -> {time_.Ref}");
         // time_.Debug();
-        playableDirector_.time = time_.Ref;
-        if (playableDirector_.state != PlayState.Playing) {
+        ref readonly var state = ref states_.Ref;
+        playableDirector_.time = state.time;
+        if (state.isPlaying) {
           playableDirector_.Play();
+        } else {
+          playableDirector_.Pause();
         }
       }
     }
