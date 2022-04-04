59653367e3a61c23d9bc8e79117da2ac486523aa
Kaede Fujisaki <kaede@hexe.net>
2022-04-04 15:34:12 +0900

    made it private

--- Assets/Scripts/Reversible/Unity/ReversibleBehaviour.cs
+++ Assets/Scripts/Reversible/Unity/ReversibleBehaviour.cs
@@ -2,21 +2,19 @@
 
 namespace Reversible.Unity {
   public abstract class ReversibleBehaviour: MonoBehaviour {
-    protected ClockHolder clockHolder;
+    private ClockHolder clockHolder_;
     protected Clock clock;
     protected PlayerInput playerInput;
     protected abstract void OnStart();
     private void Start() {
-      clockHolder = GameObject.FindGameObjectWithTag("Clock").GetComponent<ClockHolder>();
-      playerInput = clockHolder.PlayerInput;
-      clock = clockHolder.Clock;
+      clockHolder_ = GameObject.FindGameObjectWithTag("Clock").GetComponent<ClockHolder>();
+      playerInput = clockHolder_.PlayerInput;
+      clock = clockHolder_.Clock;
       OnStart();
     }
     protected abstract void OnForward();
 
-    protected virtual void OnReverse() {
-      
-    }
+    protected virtual void OnReverse() {}
     private void Update() {
       if (clock.IsTicking) {
         OnForward();
