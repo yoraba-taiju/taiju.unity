18d17e3d4db41915f8982923b7c11a05fd600fff
Kaede Fujisaki <kaede@hexe.net>
2022-04-10 02:10:30 +0900

    Collision with terrian

--- Assets/Scripts/Witch/Bullet/SoraBullet.cs
+++ Assets/Scripts/Witch/Bullet/SoraBullet.cs
@@ -18,5 +18,22 @@ namespace Witch.Bullet {
     private void OnCollisionStay2D(Collision2D other) {
       Destroy();
     }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+      OnTrigger2D(other);
+    }
+    
+    private void OnTriggerStay2D(Collider2D other) {
+      OnTrigger2D(other);
+    }
+
+    private void OnTrigger2D(Collider2D other) {
+      if (!clock.IsTicking) {
+        return;
+      }
+      if (other.gameObject.CompareTag("Terrain")) {
+        Destroy();
+      }
+    }
   }
 }
