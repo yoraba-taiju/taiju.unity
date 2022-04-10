fae679f6a4d221b8e155c8bc4b49dfde14a33a2c
Kaede Fujisaki <kaede@hexe.net>
2022-04-10 14:22:08 +0900

    renamed

--- Assets/Scripts/Enemy/Drone/Drone1.cs
+++ Assets/Scripts/Enemy/Drone/Drone1.cs
@@ -1,26 +1,30 @@
 ﻿using Reversible.Unity;
+using Reversible.Value;
 using UnityEngine;
 
 namespace Enemy.Drone {
   public class Drone1: EnemyBehaviour {
 
     [HideInInspector] public GameObject sora;
-    [SerializeField] public float shield = 10.0f;
-    [SerializeField] public GameObject explosion;
+    [SerializeField] public float initialShield = 10.0f;
+    private Sparse<float> shield_;
+    [SerializeField] public GameObject explosionEffect;
     [SerializeField] public GameObject bullet;
 
     protected override void OnStart() {
       sora = GameObject.FindWithTag("Player");
+      shield_ = new Sparse<float>(clock, initialShield);
     }
 
     protected override void OnForward() {
     }
 
     protected override void OnCollide(Collision2D collision) {
+      ref var shield = ref shield_.Mut;
       shield -= 1.0f;
       if (shield <= 0) {
         Destroy();
-        var e = Instantiate(explosion, transform.parent);
+        var e = Instantiate(explosionEffect, transform.parent);
         e.transform.localPosition = transform.localPosition;
       }
     }
--- Assets/Scripts/Enemy/Drone/Drone2.cs
+++ Assets/Scripts/Enemy/Drone/Drone2.cs
@@ -1,25 +1,28 @@
-﻿using Reversible.Unity;
+﻿using Reversible.Value;
 using UnityEngine;
 
 namespace Enemy.Drone {
   public class Drone2: EnemyBehaviour {
 
     [HideInInspector] public GameObject sora;
-    [SerializeField] public float shield = 1.0f;
-    [SerializeField] public GameObject explosion;
+    [SerializeField] public float initialShield = 1.0f;
+    private Sparse<float> shield_;
+    [SerializeField] public GameObject explosionEffect;
 
     protected override void OnStart() {
       sora = GameObject.FindWithTag("Player");
+      shield_ = new Sparse<float>(clock, initialShield);
     }
 
     protected override void OnForward() {
     }
 
     protected override void OnCollide(Collision2D collision) {
+      ref var shield = ref shield_.Mut;
       shield -= 1.0f;
       if (shield <= 0) {
         Destroy();
-        var e = Instantiate(explosion, transform.parent);
+        var e = Instantiate(explosionEffect, transform.parent);
         e.transform.localPosition = transform.localPosition;
       }
     }
