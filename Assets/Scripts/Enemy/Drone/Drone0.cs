4c22a4fe1ed0de65bab96becb90a4aa12a84eb89
Kaede Fujisaki <kaede@hexe.net>
2022-04-13 14:44:43 +0900

    Stop using MotionBehaviour

--- Assets/Scripts/Enemy/Drone/Drone0.cs
+++ Assets/Scripts/Enemy/Drone/Drone0.cs
@@ -1,20 +1,44 @@
-﻿using Reversible.Value;
+﻿using System;
+using Reversible.Value;
 using UnityEngine;
 
 namespace Enemy.Drone {
   public class Drone0: EnemyBehaviour {
+    private static readonly int Seeking = Animator.StringToHash("Seeking");
+    private static readonly int Watching = Animator.StringToHash("Watching");
+    private static readonly int Return = Animator.StringToHash("Return");
+    private static readonly int ToWatching = Animator.StringToHash("ToWatching");
 
-    [HideInInspector] public GameObject sora;
+    private GameObject sora_;
+    private Animator animator_;
+    private Rigidbody2D rigidbody_;
     [SerializeField] public float initialShield = 1.0f;
     private Sparse<float> shield_;
     [SerializeField] public GameObject explosionEffect;
 
+
     protected override void OnStart() {
-      sora = GameObject.FindWithTag("Player");
+      sora_ = GameObject.FindWithTag("Player");
+      animator_ = GetComponent<Animator>();
+      rigidbody_ = GetComponent<Rigidbody2D>();
       shield_ = new Sparse<float>(clock, initialShield);
     }
 
     protected override void OnForward() {
+      var currentHash = animator_.GetCurrentAnimatorStateInfo(1).shortNameHash;
+      var delta = (Vector2)(sora_.transform.position - transform.position);
+      if (currentHash == Seeking) {
+        if (delta.magnitude <= 6.0f) {
+          animator_.SetTrigger(ToWatching);
+        } else {
+          rigidbody_.velocity = delta.normalized * 7.0f;
+        }
+      } else if (currentHash == Watching) {
+        var d = Math.Clamp(delta.magnitude - 4.0f, -2.0f, 2.0f);
+        rigidbody_.velocity = delta.normalized * d;
+      } else if (currentHash == Return) {
+        rigidbody_.velocity = Vector2.right * 3.0f;
+      }
     }
 
     protected override void OnCollide(Collision2D collision) {
