f5977cd84a7c486d2bac015da719b9ff747ed5e5
Kaede Fujisaki <kaede@hexe.net>
2022-05-07 08:27:39 +0900

    Witch Marker experiment

---
+++ Assets/Scripts/UI/WitchMarker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI {
+  public class WitchMarker: MonoBehaviour {
+    [SerializeField] private GameObject witch_;
+    [SerializeField] private Vector3 offset_;
+    [SerializeField] private Camera Camera3d_;
+    [SerializeField] private Camera Camera2d_;
+
+    private void Start() {
+      
+    }
+
+    private void Update() {
+      var pos = Camera3d_.WorldToViewportPoint(witch_.transform.position + offset_);
+      pos.z = 10;
+      transform.position = Camera2d_.ViewportToWorldPoint(pos);
+    }
+  }
+}
