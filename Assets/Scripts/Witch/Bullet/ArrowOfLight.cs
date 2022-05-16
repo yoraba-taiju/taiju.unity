c99ff337e901359197c67f27452c1c0f6ea132f5
Kaede Fujisaki <kaede@hexe.net>
2022-05-16 20:54:07 +0900

    Arrow of light

---
+++ Assets/Scripts/Witch/Bullet/ArrowOfLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+using Reversible.Value;
+
+namespace Witch.Bullet {
+  public class ArrowOfLight: Reversible.Unity.ReversibleBehaviour {
+    [SerializeField] private GameObject arrow;
+    private Dense<Vector2> direction_;
+    protected override void OnStart() {
+      direction_.Mut = Quaternion.Euler(0, 0, Random.Range(-180, 180)) * Vector2.left;
+    }
+
+    protected override void OnForward() {
+      ref var direction = ref direction_.Mut;
+      arrow.transform.localPosition += (Vector3)direction * 20.0f;
+    }
+  }
+}
