bfa621fc5083a3bfd6f14f6b73ec464f61beb54f
Kaede Fujisaki <kaede@hexe.net>
2022-07-30 08:40:19 +0900

    follow

--- Assets/Scripts/Enemy/Drone/Drone0.cs
+++ Assets/Scripts/Enemy/Drone/Drone0.cs
@@ -46,9 +46,7 @@ namespace Enemy.Drone {
         if (delta.magnitude <= 6.0f) {
           animator_.SetTrigger(Trigger.ToWatching);
         } else {
-          var current = rigidbody_.velocity;
-          var angleDelta = VecUtil.DeltaAngle(current, delta, deltaTime * maxRotateDegreePerSecond);
-          rigidbody_.velocity =  VecUtil.Rotate(current, angleDelta).normalized * 7.0f;
+          rigidbody_.velocity =  VecUtil.Follow(rigidbody_.velocity, delta, deltaTime * maxRotateDegreePerSecond);
         }
       } else if (currentHash == State.Watching) {
         var targetPosition = soraPosition + Vector2.right * 5.0f;
--- Assets/Scripts/Utility/VecUtil.cs
+++ Assets/Scripts/Utility/VecUtil.cs
@@ -11,6 +11,27 @@ namespace Utility {
       //return Quaternion.Euler(0.0f, 0.0f, angleToRotate) * direction;
     }
 
+    public static Vector2 Follow(Vector2 currentVelocity, Vector2 target) {
+      return target.normalized * currentVelocity.magnitude;
+    }
+
+    public static Vector2 Follow(Vector2 currentVelocity, Vector2 target, float maxAngle) {
+      target = target.normalized;
+      var speed = currentVelocity.magnitude;
+      var direct = target * speed;
+      var tx = target.x;
+      var ty = target.y;
+      var c = Mathf.Cos(maxAngle * Mathf.Deg2Rad);
+      var s = Mathf.Sin(maxAngle * Mathf.Deg2Rad);
+      var limited1 = new Vector2(tx * c - s * ty, tx * s + ty * c);
+      if (Vector2.Dot(currentVelocity, direct) >= Vector2.Dot(currentVelocity, limited1)) {
+        return direct;
+      }
+      s = -s;
+      var limited2 = new Vector2(tx * c - s * ty, tx * s + ty * c);
+      return Vector2.Dot(target, limited1) >= Vector2.Dot(target, limited2) ? limited1 : limited2;
+    }
+
     public static float Atan2(float x, float y) {
       return Mathf.Atan2(y, x) * Mathf.Rad2Deg;
     }
@@ -19,12 +40,12 @@ namespace Utility {
       return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
     }
 
-    public static float DeltaAngle(Vector2 from, Vector2 to, float maxAngleDegree) {
-      return Mathf.Clamp(DeltaAngle(from, to), -maxAngleDegree, maxAngleDegree);
+    public static float DeltaAngle(Vector2 from, Vector2 to, float maxAngle) {
+      return Mathf.Clamp(DeltaAngle(from, to), -maxAngle, maxAngle);
     }
 
-    public static float DeltaAngle(float fromDegree, Vector2 to, float maxAngleDegree) {
-      return Mathf.Clamp(DeltaAngle(fromDegree, to), -maxAngleDegree, maxAngleDegree);
+    public static float DeltaAngle(float fromDegree, Vector2 to, float maxAngle) {
+      return Mathf.Clamp(DeltaAngle(fromDegree, to), -maxAngle, maxAngle);
     }
 
     public static float DeltaAngle(Vector2 from, Vector2 to) {
