adbb351c90a349fbb5ce36f200e7b524bb7689ea
Kaede Fujisaki <kaede@hexe.net>
2022-07-31 14:14:25 +0900

    rename

--- Assets/Scripts/Enemy/Drone/Drone1.cs
+++ Assets/Scripts/Enemy/Drone/Drone1.cs
@@ -1,49 +1,91 @@
-﻿using System;
-using Enemy.Bullet;
-using Reversible.Unity;
+﻿using Enemy.Bullet;
 using Reversible.Value;
 using UnityEngine;
+using Utility;
 
 namespace Enemy.Drone {
-  public class Drone1: EnemyBehaviour {
+  /**
+   * Drone1:
+   * 目的：監視用ドローン。
+   * 行動：魔女に向かい、魔女を見つけたら情報を収集し、持ち帰る。
+   */
+  public class Drone1: EnemyBehaviour, IAnimatorEventSubscriber {
     private struct State {
       public static readonly int Seeking = Animator.StringToHash("Seeking");
-      public static readonly int GivingUp = Animator.StringToHash("GivingUp");
       public static readonly int Fighting = Animator.StringToHash("Fighting");
+      public static readonly int Escaping = Animator.StringToHash("Escaping");
     }
-
+    
+    private struct Param {
+      public static readonly int NextAction = Animator.StringToHash("NextAction");
+    }
+    
+    private GameObject sora_;
     private Animator animator_;
     private Rigidbody2D rigidbody_;
 
-    [SerializeField] public float initialShield = 40.0f;
+    [SerializeField] private float initialShield = 10.0f;
+    [SerializeField] private float maxRotateDegreePerSecond = 150.0f;
+    [SerializeField] private float velocity = 7.0f;
     private Sparse<float> shield_;
+    private Sparse<int> fireCount_;
     private Dense<float> timeToFire_;
-    [SerializeField] public GameObject explosionEffect;
-    [SerializeField] public GameObject bullet;
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private GameObject bullet;
     protected override void OnStart() {
+      sora_ = GameObject.FindWithTag("Player");
       animator_ = GetComponent<Animator>();
       rigidbody_ = GetComponent<Rigidbody2D>();
       shield_ = new Sparse<float>(clock, initialShield);
+      fireCount_ = new Sparse<int>(clock, 0);
       timeToFire_ = new Dense<float>(clock, 0.3f);
     }
 
     protected override void OnForward() {
-      var currentHash = animator_.GetCurrentAnimatorStateInfo(1).shortNameHash;
-      if (currentHash == State.Seeking || currentHash == State.GivingUp) {
-        rigidbody_.velocity = Vector2.left * 5.0f;
+      var trans = transform;
+      var deltaTime = Time.deltaTime;
+      var currentHash = animator_.GetCurrentAnimatorStateInfo(0).shortNameHash;
+      var delta = (Vector2)(sora_.transform.position - trans.position);
+      var distance = delta.magnitude;
+      var currentRot = trans.localRotation;
+      var deltaAngle = VecMath.DeltaAngle(currentRot.eulerAngles.z + 180.0f, delta);
+
+      if (currentHash == State.Seeking) {
+        // Rotate to the target
+        var maxAngleDegree = maxRotateDegreePerSecond * deltaTime;
+        var moveAngleDegree = Mathf.Clamp(deltaAngle, -maxAngleDegree, maxAngleDegree);
+        var rot = currentRot * Quaternion.Euler(0, 0, moveAngleDegree);
+        trans.localRotation = rot;
+        // Set speed
+        if (distance > 10.0f) {
+          rigidbody_.velocity = rot * Vector2.left * velocity;
+        } else {
+          rigidbody_.velocity = Vector2.zero;
+        }
       } else if (currentHash == State.Fighting) {
         ref var timeToFire = ref timeToFire_.Mut;
-        timeToFire -= Time.deltaTime;
+        timeToFire -= deltaTime;
         if (timeToFire <= 0.0f) {
-          timeToFire += 0.3f;
-
-          var obj = Instantiate(bullet, transform.parent);
-          obj.transform.localPosition = transform.localPosition + Vector3.left * 2.5f;
-
-          var aim = obj.GetComponent<FixedSpeedBullet>();
-          aim.Velocity = Vector2.left * 15.0f;
+          var direction = trans.localRotation * Vector3.left;
+          var b = Instantiate(bullet, trans.parent);
+          b.transform.localPosition = trans.localPosition + direction * 1.3f;
+          var aim = b.GetComponent<FixedSpeedBullet>();
+          aim.Velocity = direction * 15.0f;
+          timeToFire = 0.3f;
         }
-        rigidbody_.velocity *= Mathf.Exp(-Time.deltaTime);
+        rigidbody_.velocity = Vector2.zero;
+      } else if (currentHash == State.Escaping) {
+        var direction = currentRot * Vector3.left;
+        rigidbody_.velocity = direction * 15.0f;
+      }
+
+      // Think Next Action!
+      if (distance >= 10.0f || deltaAngle >= 3.0f) {
+        animator_.SetInteger(Param.NextAction, 0);
+      } else if (fireCount_.Ref < 3) {
+        animator_.SetInteger(Param.NextAction, 1);
+      } else {
+        animator_.SetInteger(Param.NextAction, 2);
       }
     }
 
@@ -54,6 +96,12 @@ namespace Enemy.Drone {
         Destroy();
         var explosion = Instantiate(explosionEffect, transform.parent);
         explosion.transform.localPosition = transform.localPosition;
+      }
+    }
+
+    public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+      if (stateInfo.shortNameHash == State.Fighting) {
+        fireCount_.Mut++;
       }
     }
   }
---
+++ Assets/Scripts/Enemy/Drone/Drone3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Enemy.Bullet;
+using Reversible.Unity;
+using Reversible.Value;
+using UnityEngine;
+
+namespace Enemy.Drone {
+  public class Drone3: EnemyBehaviour {
+    private struct State {
+      public static readonly int Seeking = Animator.StringToHash("Seeking");
+      public static readonly int GivingUp = Animator.StringToHash("GivingUp");
+      public static readonly int Fighting = Animator.StringToHash("Fighting");
+    }
+
+    private Animator animator_;
+    private Rigidbody2D rigidbody_;
+
+    [SerializeField] public float initialShield = 40.0f;
+    private Sparse<float> shield_;
+    private Dense<float> timeToFire_;
+    [SerializeField] public GameObject explosionEffect;
+    [SerializeField] public GameObject bullet;
+    protected override void OnStart() {
+      animator_ = GetComponent<Animator>();
+      rigidbody_ = GetComponent<Rigidbody2D>();
+      shield_ = new Sparse<float>(clock, initialShield);
+      timeToFire_ = new Dense<float>(clock, 0.3f);
+    }
+
+    protected override void OnForward() {
+      var currentHash = animator_.GetCurrentAnimatorStateInfo(1).shortNameHash;
+      if (currentHash == State.Seeking || currentHash == State.GivingUp) {
+        rigidbody_.velocity = Vector2.left * 5.0f;
+      } else if (currentHash == State.Fighting) {
+        ref var timeToFire = ref timeToFire_.Mut;
+        timeToFire -= Time.deltaTime;
+        if (timeToFire <= 0.0f) {
+          timeToFire += 0.3f;
+
+          var obj = Instantiate(bullet, transform.parent);
+          obj.transform.localPosition = transform.localPosition + Vector3.left * 2.5f;
+
+          var aim = obj.GetComponent<FixedSpeedBullet>();
+          aim.Velocity = Vector2.left * 15.0f;
+        }
+        rigidbody_.velocity *= Mathf.Exp(-Time.deltaTime);
+      }
+    }
+
+    protected override void OnCollide(Collision2D collision) {
+      ref var shield = ref shield_.Mut;
+      shield -= 1.0f;
+      if (shield <= 0) {
+        Destroy();
+        var explosion = Instantiate(explosionEffect, transform.parent);
+        explosion.transform.localPosition = transform.localPosition;
+      }
+    }
+  }
+}
