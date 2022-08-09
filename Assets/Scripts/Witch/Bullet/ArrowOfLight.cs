375d8ff25e0a5608f7797af67c84498db1d2af30
Kaede Fujisaki <kaede@hexe.net>
2022-08-09 18:48:23 +0900

    Refactor

--- Assets/Scripts/Witch/Bullet/ArrowOfLight.cs
+++ Assets/Scripts/Witch/Bullet/ArrowOfLight.cs
@@ -1,4 +1,5 @@
-﻿using Enemy;
+﻿using System.Security.Cryptography;
+using Enemy;
 using Reversible.Unity;
 using Reversible.Value;
 using UnityEngine;
@@ -14,6 +15,7 @@ namespace Witch.Bullet {
     // Position management
     private Dense<Vector3> velocity_;
     private Sparse<bool> isTracking_;
+    private Sparse<bool> hit_;
 
     // Body
     private Transform body_;
@@ -26,6 +28,7 @@ namespace Witch.Bullet {
 
     // Management
     private Dense<float> totalTime_;
+    private float Duration { get; set; }
 
     private new void Start() {
       var self = this as ReversibleBehaviour;
@@ -43,14 +46,15 @@ namespace Witch.Bullet {
         var material = body_.GetComponent<MeshRenderer>().material;
         material.color = color;
       }
+      var trailRenderer = GetComponent<TrailRenderer>();
       {
-        var trailRenderer = GetComponent<TrailRenderer>();
         var colorGradient = trailRenderer.colorGradient;
         var colorKeys = colorGradient.colorKeys;
         colorKeys[1].color = color;
         colorGradient.colorKeys = colorKeys;
         trailRenderer.colorGradient = colorGradient;
       }
+      Duration = period + trailRenderer.time;
     }
 
     private void Track(GameObject obj) {
@@ -66,52 +70,65 @@ namespace Witch.Bullet {
       var trans = transform;
       ref var totalTime = ref totalTime_.Mut;
       totalTime += dt;
-      if (!isTracking_.Ref) {
-        // Do not track anything. Just destroy.
-        if (totalTime < 0.0f) {
-          Destroy();
-          return;
+      if (totalTime <= period) {
+        var isTracking = isTracking_.Ref;
+        if (isTracking && (target_ == null || !target_.gameObject.activeSelf)) {
+          isTracking = FindNextTarget();
         }
-      }else if (target_ == null || !target_.gameObject.activeSelf) {
-        var minDistance = float.MaxValue;
-        GameObject nextTarget = null;
-        bool found = false;
-        foreach (var other in world.LivingObjects) {
-          if (other.GetComponent<EnemyBehaviour>() == null) {
-            continue;
-          }
-          var diff = other.localPosition - trans.localPosition;
-          var distance = diff.magnitude;
-          if (distance < minDistance) {
-            minDistance = distance;
-            nextTarget = other.gameObject;
-            found = true;
-          }
+        if (isTracking) {
+          var force = Mover.TrackingForce(
+            trans.localPosition,
+            velocity,
+            target_.localPosition,
+            targetRigidbody_.velocity,
+            period - totalTime
+          );
+          velocity += force * dt;
         }
-
-        if (found) {
-          Track(nextTarget);
+        bodyRotation += bodyRotationSpeed * dt;
+        trans.localPosition += velocity * dt;
+        trans.localRotation = Quaternion.FromToRotation(Vector3.right, velocity) * Quaternion.Euler(bodyRotation, 0, 0);
+      } else if (totalTime <= Duration) {
+        var scale = (Duration - totalTime) / (Duration - period);
+        if (isTracking_.Ref) {
+          if (!hit_.Ref) {
+            targetBehaviour_.OnCollide(gameObject);
+            hit_.Mut = true;
+          } else {
+            trans.localScale = Vector3.zero;
+          }
         } else {
-          isTracking_.Mut = false;
+          trans.localScale = new Vector3(scale, scale, scale);
         }
       } else {
-        if (totalTime >= period) {
-          targetBehaviour_.OnCollide(gameObject);
-          Destroy();
-          return;
-        }
-        var force = Mover.TrackingForce(
-          trans.localPosition,
-          velocity,
-          target_.localPosition,
-          targetRigidbody_.velocity,
-          period - totalTime
-        );
-        velocity += force * dt;
+        Destroy();
       }
-      bodyRotation += bodyRotationSpeed * dt;
-      trans.localPosition += velocity * dt;
-      trans.localRotation = Quaternion.FromToRotation(Vector3.right, velocity) * Quaternion.Euler(bodyRotation, 0, 0);
+    }
+
+    private bool FindNextTarget() {
+      var minDistance = float.MaxValue;
+      var trans = transform;
+      GameObject nextTarget = null;
+      bool found = false;
+      foreach (var other in world.LivingObjects) {
+        if (other.GetComponent<EnemyBehaviour>() == null) {
+          continue;
+        }
+        var diff = other.localPosition - trans.localPosition;
+        var distance = diff.magnitude;
+        if (distance < minDistance) {
+          minDistance = distance;
+          nextTarget = other.gameObject;
+          found = true;
+        }
+      }
+      if (found) {
+        Track(nextTarget);
+      } else {
+        isTracking_.Mut = false;
+      }
+
+      return found;
     }
     protected override void OnReverse() {
       ref readonly var bodyRotation = ref bodyRotation_.Ref;
