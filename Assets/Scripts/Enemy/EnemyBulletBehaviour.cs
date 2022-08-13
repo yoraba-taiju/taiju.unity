114c1a51464ca23398bb046e62d8a39028bc748f
Kaede Fujisaki <kaede@hexe.net>
2022-08-13 16:19:13 +0900

    Simplify

--- Assets/Scripts/Enemy/EnemyBulletBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBulletBehaviour.cs
@@ -3,30 +3,35 @@ using UnityEngine;
 
 namespace Enemy {
   public abstract class EnemyBulletBehaviour : ReversibleBehaviour {
-    private int witchLayer_;
-    private int terrainLayer_;
+    private static int witchLayer_;
+    private static int terrainLayer_;
 
     private new void Start() {
       var self = this as ReversibleBehaviour;
       self.Start();
-      witchLayer_ = LayerMask.NameToLayer("Witch");
-      terrainLayer_ = LayerMask.NameToLayer("Terrain");
+      if (witchLayer_ == 0) {
+        witchLayer_ = LayerMask.NameToLayer("Witch");
+      }
+
+      if (terrainLayer_ == 0) {
+        terrainLayer_ = LayerMask.NameToLayer("Terrain");
+      }
     }
 
-    private void OnCollisionEnter2D(Collision2D col) {
-      OnCollision2D(col.gameObject);
+    private void OnCollisionEnter2D(Collision2D other) {
+      OnCollision2D(other.gameObject);
     }
 
-    private void OnCollisionStay2D(Collision2D col) {
-      OnCollision2D(col.gameObject);
+    private void OnCollisionStay2D(Collision2D other) {
+      OnCollision2D(other.gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D col) {
-      OnCollision2D(col.gameObject);
+    private void OnTriggerEnter2D(Collider2D other) {
+      OnCollision2D(other.gameObject);
     }
 
-    private void OnTriggerStay2D(Collider2D col) {
-      OnCollision2D(col.gameObject);
+    private void OnTriggerStay2D(Collider2D other) {
+      OnCollision2D(other.gameObject);
     }
 
     private void OnCollision2D(GameObject other) {
@@ -34,7 +39,7 @@ namespace Enemy {
         return;
       }
 
-      var layer = other.gameObject.layer;
+      var layer = other.layer;
       if (layer == terrainLayer_) {
         Destroy();
         return;
