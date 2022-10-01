a836c2b1a4ec7e7893b5f45ad08187f5ecd067a7
Kaede Fujisaki <kaede@hexe.net>
2022-10-01 17:46:33 +0900

    Impl cloud

---
+++ Assets/Scripts/Background/Cloud.cs
@@ -0,0 +1,38 @@
+﻿using Reversible.Unity;
+using Reversible.Value;
+using UnityEngine;
+
+namespace Background {
+  public class Cloud: ReversibleBase {
+    [SerializeField] private float speed;
+    private Dense<float> xPosition_;
+    protected new void Start() {
+      base.Start();
+      xPosition_ = new Dense<float>(clock, transform.localPosition.x);
+    }
+    protected override void OnUpdate() {
+      var trans = transform;
+      ref var x = ref xPosition_.Mut;
+      switch (x) {
+        case > 40.1f:
+          x = -40.0f;
+          break;
+        case < -40.1f:
+          x = 40.0f;
+          break;
+        default:
+          x += speed * Time.deltaTime;
+          break;
+      }
+      var pos = trans.localPosition;
+      pos.x = x;
+      trans.localPosition = pos;
+    }
+
+    public override void OnDeactivated() {
+    }
+
+    public override void OnReactivated() {
+    }
+  }
+}
