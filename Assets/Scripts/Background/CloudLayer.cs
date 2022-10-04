2d7057345435ff17ea6d70c1481e2b254ef67887
Kaede Fujisaki <kaede@hexe.net>
2022-10-04 21:15:53 +0900

    Add cloud layer shader

---
+++ Assets/Scripts/Background/CloudLayer.cs
@@ -0,0 +1,29 @@
+﻿using Reversible.Unity;
+using UnityEngine;
+
+namespace Background {
+  public class CloudLayer : ReversibleBase {
+    private Renderer renderer_;
+    private Material material_;
+
+    private struct ShaderKey {
+      public static readonly int CurrentTime = Shader.PropertyToID("_CurrentTime");
+    }
+
+    protected new void Start() {
+      base.Start();
+      renderer_ = GetComponent<MeshRenderer>();
+      material_ = renderer_.material;
+    }
+
+    protected override void OnUpdate() {
+      material_.SetFloat(ShaderKey.CurrentTime, IntegrationTime);
+    }
+
+    public override void OnDeactivated() {
+    }
+
+    public override void OnReactivated() {
+    }
+  }
+}
