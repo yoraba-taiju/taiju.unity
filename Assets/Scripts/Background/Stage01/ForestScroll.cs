0dbd1bc2faf418153d5498d2b6fec177fbbee416
Kaede Fujisaki <kaede@hexe.net>
2022-10-15 09:34:07 +0900

    Using custom shader

--- Assets/Scripts/Background/Stage01/ForestScroll.cs
+++ Assets/Scripts/Background/Stage01/ForestScroll.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 namespace Background.Stage01 {
   public class ForestScroll : ReversibleBehaviour {
-    [SerializeField] private float speed = 0.1f;
-    private Dense<Vector2> offset_;
+    private struct Params {
+      public static readonly int Offset = Shader.PropertyToID("_Offset");
+    }
+    [SerializeField] private float speed = 0.5f;
+    private Dense<float> offset_;
     private Renderer renderer_;
+    private Material material_;
 
     protected override void OnStart() {
       renderer_ = GetComponent<Renderer>();
-      offset_ = new Dense<Vector2>(clock, renderer_.material.mainTextureOffset);
+      material_ = renderer_.material;
+      offset_ = new Dense<float>(clock, 0.0f);
     }
 
     protected override void OnForward() {
       ref var offset = ref offset_.Mut;
-      offset += Vector2.left * (Time.deltaTime * speed);
-      renderer_.material.mainTextureOffset = offset;
+      offset += (Time.deltaTime * speed);
+      material_.SetFloat(Params.Offset, offset);
     }
 
     protected override void OnReverse() {
-      renderer_.material.mainTextureOffset = offset_.Ref;
+      material_.SetFloat(Params.Offset, offset_.Ref);
     }
   }
 }
