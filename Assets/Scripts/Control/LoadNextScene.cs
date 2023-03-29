4d144943ce5395d6511e10b1cdc29b11bb289a8f
Kaede Fujisaki <kaede@hexe.net>
2023-03-29 23:02:49 +0900

    いろいろ

---
+++ Assets/Scripts/Control/LoadNextScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Control {
+  public class LoadNextScene : MonoBehaviour {
+    private void Start() {
+      StartCoroutine(StartLoad());
+    }
+    IEnumerator<Object> StartLoad() {
+      var ticket = SceneManager.LoadSceneAsync("Stage");
+      while (!ticket.isDone) {
+        yield return null;
+      }
+    }
+  }
+}
