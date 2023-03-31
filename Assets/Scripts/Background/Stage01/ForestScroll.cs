b874b40cbf9ec76cebf960dbb3d9a1004457b1fc
Kaede Fujisaki <kaede@hexe.net>
2023-04-01 01:47:29 +0900

    いらん

--- Assets/Scripts/Background/Stage01/ForestScroll.cs
+++ Assets/Scripts/Background/Stage01/ForestScroll.cs
@@ -7,25 +7,21 @@ namespace Background.Stage01 {
     private struct Params {
       public static readonly int Offset = Shader.PropertyToID("_Offset");
     }
-    [SerializeField] private float speed = 0.5f;
-    private Dense<float> offset_;
+    [SerializeField] private float speed = 2.0f;
     private Renderer renderer_;
     private Material material_;
 
     protected override void OnStart() {
       renderer_ = GetComponent<Renderer>();
       material_ = renderer_.material;
-      offset_ = new Dense<float>(clock, 0.0f);
     }
 
     protected override void OnForward() {
-      ref var offset = ref offset_.Mut;
-      offset += (Time.deltaTime * speed);
-      material_.SetFloat(Params.Offset, offset);
+      material_.SetFloat(Params.Offset, IntegrationTime * speed);
     }
 
     protected override void OnReverse() {
-      material_.SetFloat(Params.Offset, offset_.Ref);
+      material_.SetFloat(Params.Offset, IntegrationTime * speed);
     }
   }
 }
